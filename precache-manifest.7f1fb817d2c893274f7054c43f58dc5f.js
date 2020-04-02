e4bd402bcbe916668b80a48ba382673f25024502
Hunter Foley <hunter0531@gmail.com>
2020-04-02 08:01:29 -0700

    for deployment

---
+++ precache-manifest.7f1fb817d2c893274f7054c43f58dc5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d3c683480dfce9388d632beb88a497",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dbd7b11809e6d873100",
+    "url": "/static/css/2.f892a075.chunk.css"
+  },
+  {
+    "revision": "a4f45ae68f15facee6ea",
+    "url": "/static/css/main.d70c4143.chunk.css"
+  },
+  {
+    "revision": "6dbd7b11809e6d873100",
+    "url": "/static/js/2.ceb06ded.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.ceb06ded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4f45ae68f15facee6ea",
+    "url": "/static/js/main.69006de1.chunk.js"
+  },
+  {
+    "revision": "6e4b79b0cf7257b19905",
+    "url": "/static/js/runtime-main.b623be60.js"
+  },
+  {
+    "revision": "eea69254e54fec82d3dcbcfac723c0b1",
+    "url": "/static/media/kt_logo_150.eea69254.png"
+  }
+]);
